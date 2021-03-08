770a27706c85e23b37f6adfaa435ab3245149b98
Andrew Baak <andrewbaak@Andrews-MacBook-Air.local>
2021-03-08 12:10:17 -0500

    Completed DKsalarymodel

--- project/src/models/DKSalarymodel.R
+++ project/src/models/DKSalarymodel.R
@@ -6,25 +6,39 @@ library(glmnet)
 #Read in  data 
 data<-fread("./project/volume/data/teamconstruction/packingdata.csv")
 
-
 modeldata<-data[,.(PredPoints,`DK salary`,`DK points`)]
 
 setnames(modeldata, c("DK points","DK salary"), c("DKpoints","DKsalary"))
+
+
+modeldata[is.na(modeldata$PredPoints)]$PredPoints<-0
+modeldata[is.na(modeldata$DKsalary)]$DKsalary<-0
+modeldata[is.na(modeldata$DKpoints)]$DKpoints<-0
+
 
 #Set seed for randomization
 set.seed(123) 
 
 #creating index so data can be split into train and test
-trainIndex <- createDataPartition(modeldata$DKpoints,p=0.75,list=FALSE, na.rm = T)
+trainIndex <- createDataPartition(modeldata$DKpoints,p=0.75,list=FALSE)
 
 #splitting data into training/testing data using the trainIndex object
-train <- data[trainIndex,] #training data (75% of data)
-test <- data[-trainIndex,] #testing data (25% of data)
+train <- modeldata[trainIndex,] #training data (75% of data)
+test <- modeldata[-trainIndex,] #testing data (25% of data)
 
-x<-modeldata[,.(DKpoints,PredPoints)]
-y_train<-modeldata$DKsalary
 
-x = as.matrix(x)
+#Make dummy variables
+dummies <- dummyVars(DKpoints ~ ., data = modeldata[,.(PredPoints,DKsalary,DKpoints)])
+train_dummies = predict(dummies, newdata = train[,.(PredPoints,DKsalary,DKpoints)])
+test_dummies = predict(dummies, newdata = test[,.(PredPoints,DKsalary,DKpoints)])
+
+
+x = as.matrix(train_dummies)
+y_train = train$DKpoints
+
+x_test = as.matrix(test_dummies)
+y_test = test$DKpoints
+
 
 #Model
 
@@ -40,11 +54,22 @@ ridge_reg = glmnet(x, y_train, nlambda = 25, alpha = 0, family = 'gaussian', lam
 summary(ridge_reg)
 
 
-predictions = predict(glmnet, newdata = test)
-eval_metrics(glmnet, test, predictions, target = 'DK Salary')
+predictions = predict(ridge_reg, s = optimal_lambda, newx = x_test)
 
-
-
-
-
+# Compute R^2 from true and predicted values
+eval_results <- function(true, predicted, df) {
+  SSE <- sum((predicted - true)^2)
+  SST <- sum((true - mean(true))^2)
+  R_square <- 1 - SSE / SST
+  RMSE = sqrt(SSE/nrow(df))
+  
+  
+  # Model performance metrics
+  data.frame(
+    RMSE = RMSE,
+    Rsquare = R_square
+  )
+}
+# Prediction and evaluation on test data
+eval_results(y_test, predictions, test)
 
